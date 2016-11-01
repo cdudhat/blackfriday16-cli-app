7fe3a19a79b0a6b9d4e3674c75756555f65a8070
Chirag Dudhat <cdudhat@gmail.com>
2016-11-01 04:08:22 +0000

    Organized variable names to make more sense

--- lib/base_scraper.rb
+++ lib/base_scraper.rb
@@ -7,44 +7,43 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
 
-    deal_index = []
+    vendor_index = []
 
-    deal_page = index_page.css("div.simple-vendor-grid div.line.grid-line.content-line").each do |deal_line|
-      deal_line.css("div.unit.tile.size1of3").each do |deal|
-        if deal.css("div.content-box-hd").css("a").any?
-          deal_details = {
-            :name => deal.css("div.content-box-hd").css("a").text,
-            :page_url => deal.css("div.content-box-hd").css("a").attribute("href").value
+    deal_page = index_page.css("div.simple-vendor-grid div.line.grid-line.content-line").each do |vendor_line|
+      vendor_line.css("div.unit.tile.size1of3").each do |vendor|
+        if vendor.css("div.content-box-hd").css("a").any?
+          vendor_details = {
+            :vendor_name => vendor.css("div.content-box-hd").css("a").text,
+            :vendor_url => vendor.css("div.content-box-hd").css("a").attribute("href").value
           }
         else
           break
         end
         #binding.pry
-        deal_index << deal_details
+        vendor_index << vendor_details
       end
     end
-    deal_index
+    vendor_index
   end
 
-  def self.scrape_profile_page(page_url)
-    deals_page = Nokogiri::HTML(open(page_url))
+  def self.scrape_vendor_page(vendor_url)
+    deals_page = Nokogiri::HTML(open(vendor_url))
 
-    page_hash = {}
-    specific_deals = []
+    prev_page = nil
+    next_page = nil
+    full_page_deals = []
 
     #Compile social media links
     main_container = deals_page.css("div.main.browsecontent")
     main_container.css("div.content-view.content-box.content-summary").each do |link|
       individual_deal = []
-      deal_details = link.css("div.unit.size3of4").css("h3.headline-xlarge a").text
-      price_details = link.css("div.unit.size3of4").css("div.content-call-out").text
-      price_details = "FREE" if price_details == ""
-      individual_deal << deal_details << price_details
-      specific_deals << individual_deal
+      deal_detail = link.css("div.unit.size3of4").css("h3.headline-xlarge a").text
+      price_detail = link.css("div.unit.size3of4").css("div.content-call-out").text
+      price_detail = "FREE" if price_detail == ""
+      individual_deal << deal_detail << price_detail
+      full_page_deals << individual_deal
     end
 
-    prev_page = nil
-    next_page = nil
     pager = main_container.css("div.pager")
     prev_page = pager.css("a.pager-end[rel=prev]").attribute("href").value if pager.css("a.pager-end[rel=prev]").any?
     next_page = pager.css("a.pager-end[rel=next]").attribute("href").value if pager.css("a.pager-end[rel=next]").any?
@@ -52,22 +51,10 @@ class Scraper
     deal_page_complete = {
       :prev_page_link => prev_page,
       :next_page_link => next_page,
-      :deals => specific_deals
+      :deals => full_page_deals
     }
 
     deal_page_complete
     binding.pry
   end
-
-  #  #Profile Quote
-  #  quote_container = profile_page.css("div.vitals-text-container")
-  #  profile[:profile_quote] = quote_container.css("div.profile-quote").text
-
-  #  #Bio
-  #  detail_container = profile_page.css("div.details-container")
-  #  profile[:bio] = detail_container.css("div.bio-content.content-holder div.description-holder p").text
-
-  #  profile
-  #end
-
 end
--- spec/base_scraper_spec.rb
+++ spec/base_scraper_spec.rb
@@ -1,21 +1,19 @@
 require_relative "../lib/base_scraper.rb"
 
 describe "#scrape_index_page" do
-  it "is a class method that scrapes the student index page and a returns an array of hashes in which each hash represents one student" do
+  it "is a class method that scrapes the index page and a returns an array of hashes in which each hash represents one deal vendor" do
     index_url = "http://dealnews.com/black-friday/index.html"
-    scraped_students = Scraper.scrape_index_page(index_url)
-    expect(scraped_students).to be_a(Array)
-    expect(scraped_students.first).to have_key(:page_url)
-    expect(scraped_students.first).to have_key(:name)
-    #expect(scraped_students).to include(student_index_array[0], student_index_array[1], student_index_array[2])
+    scraped_index_page = Scraper.scrape_index_page(index_url)
+    expect(scraped_index_page).to be_a(Array)
+    expect(scraped_index_page.first).to have_key(:vendor_url)
+    expect(scraped_index_page.first).to have_key(:vendor_name)
   end
 end
 
-describe "#scrape_profile_page" do
-  it "is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student" do
-    profile_url = "http://dealnews.com/black-friday/s5843/Dollar-General/"
-    scraped_student = Scraper.scrape_profile_page(profile_url)
-    expect(scraped_student).to be_a(Array)
-    #expect(scraped_student).to match(student_joe_hash)
+describe "#scrape_vendor_page" do
+  it "is a class method that scrapes a vendor's profile page and returns a hash of attributes for that deal page" do
+    page_url = "http://dealnews.com/black-friday/s5843/Dollar-General/"
+    scraped_page = Scraper.scrape_vendor_page(page_url)
+    expect(scraped_page).to be_a(Hash)
   end
 end
